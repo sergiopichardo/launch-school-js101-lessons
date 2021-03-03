e25064ab3b3b5bf330320846f562d77794dbe85b
Sergio Pichardo <sp88work@gmail.com>
2021-03-03 14:13:40 -0600

    create calculator.js program file

---
+++ 05_calculator/calculator.js
@@ -0,0 +1,10 @@
+/*
+Walk-through: calculator
+
+Our first program in this course will be a command line calculator program that will:
+- Ask the user for two numbers.
+- Ask the user for the type of operation to perform: add, subtract, multiply or divide.
+- Perform the calculation and display the result.
+
+*/
+
